e5194b4035dbe6f38f36c7dc352947293b94d55b
Dominik Gałenza <dominik.galenza@gmail.com>
2021-12-29 09:37:25 +0100

    Added music

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,30 +6,30 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject explosionVFX;
     [SerializeField] GameObject hitVFX;
-    [SerializeField] int scorePerHit = 50;
+    [SerializeField] int scorePerKill = 50;
     [SerializeField] int hitPoints = 100;
     [SerializeField] int damagePerHit = 25;
 
     ScoreBoard scoreBoard;
-    GameObject parent;
+    GameObject parentGameObject;
 
     void Start()
     {
         scoreBoard = FindObjectOfType<ScoreBoard>();
-        parent = GameObject.FindWithTag("SpawnAtRuntime");
+        parentGameObject = GameObject.FindWithTag("SpawnAtRuntime");
         Rigidbody rb = gameObject.AddComponent<Rigidbody>();
         rb.useGravity = false;
     }
     void OnParticleCollision(GameObject other) 
     {
-        scoreBoard.IncreaseScore(scorePerHit);
         hitPoints -= damagePerHit;
         GameObject hitvfx = Instantiate(hitVFX, transform.position, Quaternion.identity);
-        hitvfx.transform.parent = parent.transform;
+        hitvfx.transform.parent = parentGameObject.transform;
         if(hitPoints <= 0)
         {
+            scoreBoard.IncreaseScore(scorePerKill);
             GameObject explosionvfx = Instantiate(explosionVFX, transform.position, Quaternion.identity);
-            explosionvfx.transform.parent = parent.transform;
+            explosionvfx.transform.parent = parentGameObject.transform;
             Destroy(gameObject);
         }
     }
